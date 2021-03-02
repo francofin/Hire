fd207fb59444f7df035798b0c1efe3b812d67fad
fintank <finapptank@gmail.com>
2021-03-02 00:41:15 -0500

    updated secret to exclude from public

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const secret = 'bahbsdhbfhkfbskdj,fnkjbgifkbgvdjkdjsf,vkgzgjdfabzdfn,jzbnlkjbnjkd';
+const secret = process.env.SECRET;
 const expiration = '2h';
 
 module.exports = {

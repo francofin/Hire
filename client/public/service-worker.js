b38aa24a50b5c5fd880bb6458e3f9404b1b075cd
fintank <finapptank@gmail.com>
2021-03-02 20:29:38 -0500

    updated sw.js

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -5,24 +5,7 @@ const DATA_CACHE_NAME = "data-cache-v1"
 
 const FILES_TO_CACHE = [
   '/',
-  '/index.html',
-  '/favicon.ico',
-  '/manifest.json',
-  '/assets/css/style.css',
-  '/assets/js/loadImages.js',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/1.jpg',
-  '/assets/images/2.jpg',
-  '/assets/images/3.jpg',
-  '/assets/images/4.jpg',
-  '/assets/images/5.jpg',
-  '/assets/images/6.jpg',
-  '/assets/images/7.jpg',
-  '/assets/images/8.jpg',
-  '/assets/images/9.jpg'
+
   ];
 
   //installation
@@ -40,9 +23,9 @@ const FILES_TO_CACHE = [
   //activation
   self.addEventListener("activate", function(activateEvent){
     activateEvent.waitUntil(
-      caches.keys().then(johnsKeylis=>{ //keyList == ["my-site-cache-v1", "my-site-cache-v2", "my-site-cache-v3"....]
+      caches.keys().then(keyList=>{ //keyList == ["my-site-cache-v1", "my-site-cache-v2", "my-site-cache-v3"....]
         return Promise.all(
-          johnsKeylis.map(key=>{
+          keyList.map(key=>{
             if(key !== CACHE_NAME && key !== DATA_CACHE_NAME){
               console.log("remove old cache data", key)
               return caches.delete(key)
